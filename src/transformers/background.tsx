506afb8851dc858a404631a51c585b2e4fde890b
yiminghe <yiminghe@gmail.com>
2017-08-03 23:47:01 +0800

    ci

--- src/transformers/background.tsx
+++ src/transformers/background.tsx
@@ -6,7 +6,9 @@ export default function transform(name, value, { warning }) {
   for (let i = 0; i < parts.length; i++) {
     const p = parts[i];
     if (p.charAt(0) === '#' || p.slice(0, 3) === 'rgb') {
-      return { backgroundColor: p };
+      return {
+        backgroundColor: p,
+      };
     }
   }
   warning(name, value);
